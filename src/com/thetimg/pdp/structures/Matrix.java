a69f08984fdf91d24d17560e81934677e6fd764f
Tim Gerstel <tgerstel@umass.edu>
2017-01-02 19:26:21 -0500

    Added getSubmatrix() and additional getters to Matrix.java.  Added recursive determinant algorithm to SquareMatrix

--- src/com/thetimg/pdp/structures/Matrix.java
+++ src/com/thetimg/pdp/structures/Matrix.java
@@ -40,12 +40,12 @@ public class Matrix {
 		size = rows * cols;
 	}
 	
-	public void setValue(int row, int col, double value){
-		if(row > rows-1 || col > cols-1){
+	public void setValue(int rowIndex, int colIndex, double value){
+		if(rowIndex > rows-1 || colIndex > cols-1){
 			//Error - index out of bound
 			throw new ArrayIndexOutOfBoundsException();
 		}
-		mat[row][col] = value;
+		mat[rowIndex][colIndex] = value;
 	}
 	
 	//Set values via regex separated string
@@ -53,21 +53,47 @@ public class Matrix {
 		String[] vals = input.split(regex);
 		if(vals.length != size){
 			//Error - Number if inputs is not equal to size of matrix
-		}
-		int valInd = 0;
-		for(int i = 0; i < rows; i++){
-			for(int j = 0; j < cols; j++){
-				mat[i][j] = Double.parseDouble(vals[valInd++]);
+		} else {
+			int valInd = 0;
+			for(int i = 0; i < rows; i++){
+				for(int j = 0; j < cols; j++){
+					mat[i][j] = Double.parseDouble(vals[valInd++]);
+				}
 			}
 		}
 	}
 	
-	public double getValue(int row, int col){
-		return mat[row][col];
+	public double getValue(int rowIndex, int colIndex){
+		return mat[rowIndex][colIndex];
 	}
 	
 	public int getSize(){
 		return size;
+	}
+	
+	public int getRows(){
+		return rows;
+	}
+	
+	public int getCols(){
+		return cols;
+	}
+	
+	public Matrix getSubmatrix(int rowIndex, int colIndex){
+		StringBuilder vals = new StringBuilder();
+		double[][] sub = new double[rows-1][cols-1];
+		Matrix ret = new Matrix(sub);
+		for(int i = 0; i < rows; i++){
+			if(i != rowIndex){
+				for(int j = 0; j < cols; j++){
+					if(j != colIndex){
+						vals.append(mat[i][j] + ",");
+					}
+				}
+			}
+		}
+		ret.setValues(vals.toString(), ",");
+		return ret;
 	}
 	
 	public void toConsole(){
--- src/com/thetimg/pdp/structures/SquareMatrix.java
+++ src/com/thetimg/pdp/structures/SquareMatrix.java
@@ -4,6 +4,9 @@ public class SquareMatrix extends Matrix{
 	
 	public SquareMatrix(double[][] mat){
 		super(mat);
+		if(rows != cols){
+			//Error - Cannot be defined as square matrix
+		}
 	}
 
 	public SquareMatrix(int a) {
@@ -16,6 +19,29 @@ public class SquareMatrix extends Matrix{
 			ret = ret + mat[i][i];
 		}
 		return ret;
+	}
+	
+	public double getDet(){
+		return getDet(this);
+	}
+	
+	private double getDet(Matrix m){
+		if(m.getCols() == 1){
+			return m.getValue(0, 0);
+		} else {
+			double ret = 0;
+			int sign = 1;
+			for(int i = 0; i < m.getCols(); i++){
+				Matrix sub = m.getSubmatrix(0, i);
+				if(i%2 == 0){
+					sign = 1;
+				} else {
+					sign = -1;
+				}
+				ret += sign * m.getValue(0, i) * getDet(sub);
+			}
+			return ret;
+		}
 	}
 
 }
