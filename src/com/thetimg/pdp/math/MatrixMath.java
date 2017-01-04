32b2c1ece41fe7327c16902af3716857285c4e34
Tim Gerstel <tgerstel@umass.edu>
2017-01-04 17:37:22 -0500

    Added add and subtract functions

--- src/com/thetimg/pdp/math/MatrixMath.java
+++ src/com/thetimg/pdp/math/MatrixMath.java
@@ -1,21 +1,71 @@
 package com.thetimg.pdp.math;
 
 import com.thetimg.pdp.structures.Matrix;
+import com.thetimg.pdp.structures.SquareMatrix;
 
 public class MatrixMath {
 	
-	public Matrix add(Matrix m1, Matrix m2){
+	//returns new matrix equal to m1+m2
+	public static Matrix add(Matrix m1, Matrix m2){
 		if(m1.getRows() != m2.getRows() && m1.getCols() != m2.getCols()){
 			//Error - Matricies must be same dimensions
 			return new Matrix(null);
 		} else {
 			double[][] ret = new double[m1.getRows()][m1.getCols()];
-			for(int i = 0; i < m1.getCols(); i++){
+			for(int i = 0; i < m1.getRows(); i++){
 				for(int j = 0; j < m2.getCols(); j++){
 					ret[i][j] = m1.getValue(i, j) + m2.getValue(i, j);
 				}
 			}
 			return new Matrix(ret);
+		}
+	}
+	
+	//returns new matrix equal to m1+m2
+	public static SquareMatrix add(SquareMatrix m1, SquareMatrix m2){
+		if(m1.getRows() != m2.getRows() && m1.getCols() != m2.getCols()){
+			//Error - Matricies must be same dimensions
+			return new SquareMatrix(null);
+		} else {
+			double[][] ret = new double[m1.getRows()][m1.getCols()];
+			for(int i = 0; i < m1.getRows(); i++){
+				for(int j = 0; j < m2.getCols(); j++){
+					ret[i][j] = m1.getValue(i, j) + m2.getValue(i, j);
+				}
+			}
+			return new SquareMatrix(ret);
+		}
+	}
+	
+	//Returns new matrix equal to m1-m2
+	public static Matrix sub(Matrix m1, Matrix m2){
+		if(m1.getRows() != m2.getRows() && m1.getCols() != m2.getCols()){
+			//Error - Matricies must be same dimensions
+			return new Matrix(null);
+		} else {
+			double[][] ret = new double[m1.getRows()][m1.getCols()];
+			for(int i = 0; i < m1.getRows(); i++){
+				for(int j = 0; j < m2.getCols(); j++){
+					ret[i][j] = m1.getValue(i, j) - m2.getValue(i, j);
+				}
+			}
+			return new Matrix(ret);
+		}
+	}
+	
+	//Returns new matrix equal to m1-m2
+	public static SquareMatrix sub(SquareMatrix m1, SquareMatrix m2){
+		if(m1.getRows() != m2.getRows() && m1.getCols() != m2.getCols()){
+			//Error - Matricies must be same dimensions
+			return new SquareMatrix(null);
+		} else {
+			double[][] ret = new double[m1.getRows()][m1.getCols()];
+			for(int i = 0; i < m1.getRows(); i++){
+				for(int j = 0; j < m2.getCols(); j++){
+					ret[i][j] = m1.getValue(i, j) - m2.getValue(i, j);
+				}
+			}
+			return new SquareMatrix(ret);
 		}
 	}
 
--- src/com/thetimg/pdp/test/MatrixMathTest.java
+++ src/com/thetimg/pdp/test/MatrixMathTest.java
@@ -8,6 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
+import com.thetimg.pdp.math.MatrixMath;
 import com.thetimg.pdp.structures.Matrix;
 import com.thetimg.pdp.structures.SquareMatrix;
 
@@ -15,26 +16,30 @@ public class MatrixMathTest {
 	@Rule
 	public Timeout globalTimeout = new Timeout(1000L, TimeUnit.MILLISECONDS);
 	
-	private Matrix A, B;
+	private Matrix A, B, E;
 	private SquareMatrix C,D;
 	
-	double[][] valsA = new double[][]{
+	private double[][] valsA = new double[][]{
 		{ 11,22,33,44 },
 		{ 55,66,77,88 },
 	};
-	double[][] valsB = new double[][]{
+	private double[][] valsB = new double[][]{
 		{ 12,15,19,-20 },
 		{ 16,-20,40,10 },
 		{ 31,22,-76,2  },
 		{ 5,65,82,7    }
 	};
-	double[][] valsC = new double[][]{
+	private double[][] valsC = new double[][]{
 		{ 1,2 },
 		{ 3,4 }
 	};
-	double[][] valsD = new double[][]{
+	private double[][] valsD = new double[][]{
 		{ 5,6 },
 		{ 7,8 }
+	};
+	private double[][] valsE = new double[][]{
+		{ 11,22,33,44 },
+		{ 55,66,77,88 }, 
 	};
 	
 	@Before
@@ -43,6 +48,21 @@ public class MatrixMathTest {
 		B = new Matrix(valsB);
 		C = new SquareMatrix(valsC);
 		D = new SquareMatrix(valsD);
+		E = new Matrix(valsE);
+	}
+	
+	@Test
+	public void testAdd(){
+		SquareMatrix CD = MatrixMath.add(C, D);
+		assertEquals(6, CD.getValue(0, 0), 0);
+		assertEquals(8, CD.getValue(0, 1), 0);
+		assertEquals(10, CD.getValue(1, 0), 0);
+		assertEquals(12, CD.getValue(1, 1), 0);
+		Matrix AE = MatrixMath.add(A, E);
+		assertEquals(22, AE.getValue(0, 0), 0);
+		assertEquals(44, AE.getValue(0, 1), 0);
+		assertEquals(110, AE.getValue(1, 0), 0);
+		assertEquals(132, AE.getValue(1, 1), 0);
 	}
 
 }
