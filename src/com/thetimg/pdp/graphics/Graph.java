1dc679a2e3af0ca74a9be8524a41fb3f59554cd4
Tim Gerstel <tgerstel@umass.edu>
2017-01-17 20:21:21 -0500

    Added package for graphics

---
+++ src/com/thetimg/pdp/graphics/Graph.java
@@ -0,0 +1,18 @@
+package com.thetimg.pdp.graphics;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Graph extends JPanel {
+	
+	private int width = 800;
+	private int height = 400;
+	private int padding = 25;
+	private int labelPadding = 25;
+	private int pointW = 5;
+	private Color line = new Color(230, 55, 55, 200);
+	private Color point = new Color(0, 0, 0, 200);
+	private Color grid = new Color(200, 200, 200, 200);
+
+}
